7d485301b8bf6901dc0b28b17d8f54be81044c2b
KUSHAL RAJ PAREEK <115624967+KUSHAL-RAJ-PAREEK@users.noreply.github.com>
2024-08-29 11:52:32 +0530

    error fix

--- frontend/src/pages/Blog.tsx
+++ frontend/src/pages/Blog.tsx
@@ -8,7 +8,7 @@ export const Blog = () =>{
     const {loading, blog} = useBlog({
         id: id || ""
     });
-    if(loading){
+    if(loading || !blog){
         return <div>
             <Appbar name={"User"}/>
             <div className="h-screen flex flex-col justify-center">
